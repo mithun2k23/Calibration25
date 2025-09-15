1287318455256a29844b045e768c410b9aa1518c
mithun2k23 <mithunsr2023@gmail.com>
2025-09-15 14:40:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const eventDate = new Date("2025-09-15T14:40:00").getTime();
+const eventDate = new Date("2025-09-15T15:00:00").getTime();
 
 const countdownEl = document.getElementById("countdown");
 const revealBtn = document.getElementById("revealBtn");
@@ -61,4 +61,5 @@ revealBtn.addEventListener("click", () => {
     console.log("Autoplay blocked for poster music:", err);
   });
 });
+
 
